d2160bb085ea906fe4e06bfb6326ea4daa39a3ff
Malik Elbadri <Elbadrimalik@gmail.com>
2023-09-10 20:38:08 +0300

    Added overlay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SectionComponent } from './section/section.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    SectionComponent
+    SectionComponent,
+    OverlayComponent
   ],
   imports: [
     BrowserModule,
