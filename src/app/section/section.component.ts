14da17be8e4150b1a72507b681b905f39edf61fb
Malik Elbadri <Elbadrimalik@gmail.com>
2023-09-10 20:25:57 +0300

    Added layout and snap scroll

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.scss']
+  styleUrls: ['./section.component.scss'],
 })
 export class SectionComponent {
-
+  @Input() src: string = '';
 }
