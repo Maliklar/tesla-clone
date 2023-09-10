27ba47af687c65c61e7803eb903e8d27b5b11fc0
Malik Elbadri <MalikCura@users.noreply.github.com>
2023-09-10 18:22:44 +0300

    Added fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
