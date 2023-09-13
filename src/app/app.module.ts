6378afdc1d29cc2a456c24b0da41346d136faaef
Malik Elbadri <Elbadrimalik@gmail.com>
2023-09-13 19:41:30 +0300

    Added menu layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SectionComponent } from './section/section.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, SectionComponent],
+  declarations: [AppComponent, NavigationComponent, SectionComponent, NavMenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
