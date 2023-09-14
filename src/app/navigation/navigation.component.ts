dc4114c2c356721f12fb0bdf84a34071a0114792
Malik Elbadri <Elbadrimalik@gmail.com>
2023-09-14 18:42:55 +0300

    Added state to nav section

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+export enum MenuTypeEnum {
+  vehicles,
+  energy,
+  charging,
+  discover,
+}
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
+  readonly menuType = MenuTypeEnum;
+  menuItems = [
+    {
+      title: MenuTypeEnum.vehicles,
+      active: false,
+    },
+    {
+      title: MenuTypeEnum.energy,
+      active: false,
+    },
+    {
+      title: MenuTypeEnum.charging,
+      active: false,
+    },
+    {
+      title: MenuTypeEnum.discover,
+      active: false,
+    },
+  ];
+  hoverHandler(title: MenuTypeEnum) {
+    const activeMenu = this.menuItems.find((i) => i.title === title);
+    if (!activeMenu) return;
+    activeMenu.active = true;
 
+    this.menuItems.forEach((i) => {
+      i.active = false;
+      if (i.title === title) i.active = true;
+    });
+
+    console.log(this.menuItems);
+  }
 }
